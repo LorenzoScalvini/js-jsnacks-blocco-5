f4cec2132c43cf128a475a5c4cc632e8a9d2c355
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2024-10-30 15:29:43 +0100

    ADD: SNACK-04 CORREZIONE

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,7 +5,7 @@ const posts = [
   { author: "Enrico", date: "09/12/2021", content: "lorem ipsum..." },
 ];
 
-// Crea un nuovo array coi nomi di tutti gli autori
+// Crea un nuovo array coi nomi di tutti gli autori.
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
 
 const authors = post.map((post) => post.author);
